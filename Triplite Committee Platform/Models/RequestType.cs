6cfd584f9916f34fb2e75fa1f78f0c55dfdee34a
omaradawi10 <tu4100138@taibahu.edu.sa>
2024-03-08 17:29:43 +0300

    Add RequestType Table Model

---
+++ Triplite Committee Platform/Models/RequestType.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace Triplite_Committee_Platform.Models
+{
+
+    public class RequestType
+    {
+        [Key] public int RequestTypeID { get; set; }
+        [Required] public string RequestTypeName { get; set; }
+       // public List<BoardModel> Board { get; set; } = new List<BoardModel>();
+    }
+}
+//ddddd
