1074c3cde4fd74814b1927a63a647914763bcc7d
SalehAljohani <WeirdShock@gmail.com>
2024-03-08 18:14:27 +0300

    renamed user model to > UserModel **forgot to add Model to User

--- Triplite Committee Platform/Models/RolesModels.cs
+++ Triplite Committee Platform/Models/RolesModels.cs
@@ -2,7 +2,7 @@
 
 namespace Triplite_Committee_Platform.Models
 {
-    public class Roles
+    public class RolesModels
     {
         [Key] public int RoleID { get; set; }
         [Required] public string RoleName { get; set; }
