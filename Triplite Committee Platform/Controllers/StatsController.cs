1fd07403a0db58c9120e320272d1b22219052a67
MutazBu <moatazbusati2@gmail.com>
2024-03-21 02:54:06 +0300

    The most significant changes involve the addition of a new `StatsController` class and modifications to the `Index.cshtml` file. The `StatsController` class, added to the `Triplite_Committee_Platform.Controllers` namespace, calculates and stores various statistics related to boards, such as the total number, completed number, uncompleted number, and completion percentage. These statistics are then displayed in the `Index.cshtml` file, which also includes a visual representation of the completion percentage.
    Changes:
    
    1. A new `StatsController` class has been added to the `Triplite_Committee_Platform.Controllers` namespace. This class inherits from the `Controller` class provided by the `Microsoft.AspNetCore.Mvc` namespace.
    2. The `StatsController` class has a private field `_context` of type `AppDbContext`. This field is initialized in the constructor of the `StatsController` class, which takes an `AppDbContext` object as a parameter.
    3. The `StatsController` class has an `Index` method that calculates the total number of boards, the number of completed boards, and the number of uncompleted boards. These values are then stored in the `ViewData` dictionary with the keys "totalBoard", "CompletedBoards", and "unCompletedBoards" respectively.
    4. The `Index` method also calculates a completion percentage, which is the ratio of completed boards to total boards, and stores this value in the `ViewBag` with the key "CompletionPercentage".
    5. The `Index` method finally returns a view.
    6. In the `Index.cshtml` file, a layout is set using the `_Layout.cshtml` file located in the `Views/Shared` directory.
    7. The `Index.cshtml` file displays the total number of boards, the number of completed boards, and the number of uncompleted boards in `h1` tags.
    8. The `Index.cshtml` file also contains a div with class "chart" that visually represents the completion percentage. The div has two child divs: one with class "completed" that represents the completed boards and one with class "remaining" that represents the uncompleted boards. The width of these divs is set to the completion percentage and the remaining percentage respectively.

---
+++ Triplite Committee Platform/Controllers/StatsController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Triplite_Committee_Platform.Data;
+using Triplite_Committee_Platform.Models;
+
+
+
+namespace Triplite_Committee_Platform.Controllers
+{
+    public class StatsController : Controller
+    {
+
+        private readonly AppDbContext _context;
+
+        public StatsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var totalBoard = _context.Board.Count();
+			var completedBoards = _context.Board.Where(board => board.ReqStatus == "Completed").Count();
+			var unCompletedBoards = _context.Board.Where(board => board.ReqStatus == "Uncompleted").Count();
+
+
+
+			//var totalCollege = _context.College.Count();
+
+
+
+			ViewData["totalBoard"] = totalBoard;
+			ViewData["CompletedBoards"] = completedBoards;
+			ViewData["unCompletedBoards"] = unCompletedBoards;
+			                                              
+			var completionPercentage = (double)105 / 167 * 100;  //replace 105 to CompletedBoards and 167 to totalBoard
+
+			ViewBag.CompletionPercentage = completionPercentage;
+
+
+			return View();
+        }
+    }
+}
